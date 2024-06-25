2097944c36ac3c0ed90b2bfa4099fe78d7c4c048
Ehtisham-Hassaan <f223407@cfd.nu.edu.pk>
2024-06-25 07:28:01 -0700

    all basic function's of calculator

--- Readme.md
+++ Readme.md
@@ -1,2 +1,4 @@
 
 This project was created from local
+
+1- i have added 
--- app.js
+++ app.js
@@ -6,15 +6,20 @@ let button = document.querySelectorAll('.btn');
 let past = [];
 let solution =0;
 let result ;
+let Ans= '0';
 let screen = document.querySelector('input');
 
 let solve = document.querySelector('#Total');
 
 solve.addEventListener('click', () => {
     console.log('solve');
+    solution= solution.replace('x','*');
+    solution= solution.replace('Ans',Ans);
+
     solution = eval(solution);
     past.push (solution);
-    screen.value = solution;
+    solution=solution.toString();
+    screen.value = Ans=solution;
 })
 
 for (btn of button) {
@@ -24,7 +29,29 @@ for (btn of button) {
             solution = this.innerText;
         }
         else{
-            solution= `${solution}${this.innerText}`;
+            switch (this.innerText) {
+                case 'C':
+                    solution = solution.substring(0, solution.length-1);
+                  break;
+                case 'Rad':
+                    console.log('Add radian function')
+                  break;
+                case 'Deg':
+                    console.log('Add Degree function')
+                  break;
+                case 'x!':
+                    solution= `${solution}!`;
+                    break;
+                case 'x^y':
+                    solution= `${solution}^`;
+                  break;
+                case 'ln':
+                    solution= `${solution}ln(`;
+                  break;
+
+                default:
+                  solution= `${solution}${this.innerText}`;//to used soultion as string
+              }
         }
         screen.value = solution;
     });

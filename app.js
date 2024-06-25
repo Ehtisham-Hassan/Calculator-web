358aedca83dd42ca6aae93a459d67876fa080cc6
Ehtisham-Hassaan <f223407@cfd.nu.edu.pk>
2024-06-25 03:46:27 -0700

    made calculate basic funtion result on screen

--- app.js
+++ app.js
@@ -14,20 +14,19 @@ solve.addEventListener('click', () => {
     console.log('solve');
     solution = eval(solution);
     past.push (solution);
-
+    screen.value = solution;
 })
 
 for (btn of button) {
     btn.addEventListener('click', function ()  {
         console.log(this.innerText);
-        screen.value = this.innerText;
         if(solution ==0){
             solution = this.innerText;
         }
         else{
             solution= `${solution}${this.innerText}`;
         }
-    });    
-    
+        screen.value = solution;
+    });
 }
 

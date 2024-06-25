1296eb757dba6ae1b7e74b61da240b2f51116917
Ehtisham-Hassaan <f223407@cfd.nu.edu.pk>
2024-06-25 03:38:14 -0700

    made calculate function basic

---
+++ app.js
@@ -0,0 +1,33 @@
+let button = document.querySelectorAll('.btn');
+
+// let calfunction =  function (btn)  {
+//     console.log(btn.innerText);
+// }
+let past = [];
+let solution =0;
+let result ;
+let screen = document.querySelector('input');
+
+let solve = document.querySelector('#Total');
+
+solve.addEventListener('click', () => {
+    console.log('solve');
+    solution = eval(solution);
+    past.push (solution);
+
+})
+
+for (btn of button) {
+    btn.addEventListener('click', function ()  {
+        console.log(this.innerText);
+        screen.value = this.innerText;
+        if(solution ==0){
+            solution = this.innerText;
+        }
+        else{
+            solution= `${solution}${this.innerText}`;
+        }
+    });    
+    
+}
+
